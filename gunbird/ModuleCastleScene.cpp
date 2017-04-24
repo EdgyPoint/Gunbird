d18a5482219dcf5660adb9dfcf617372f052d165
Cristian <cristian.palos98@gmail.com>
2017-04-24 18:07:03 +0200

    Mitic morter in the path erased

--- gunbird/ModuleCastleScene.cpp
+++ gunbird/ModuleCastleScene.cpp
@@ -134,8 +134,7 @@ bool ModuleCastleScene::Start()
 
 	App->enemies->AddEnemy(ENEMY_TYPES::TURRETCOPTER, 100, -470, 0);
 	App->enemies->AddEnemy(ENEMY_TYPES::CASTLEMORTAR, 168, -187, 0);
-	App->enemies->AddEnemy(ENEMY_TYPES::CASTLEMORTAR, 100, 50, 0);
-
+	
 	graphics2 = App->textures->Load("assets/images/Castle Upper Background.png");
 	graphics = App->textures->Load("assets/images/Castle Background.png");
 	knight = App->textures->Load("assets/images/knight.png");
