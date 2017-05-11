842dc3ce18c583ee1acf205585d9b32bce3ddc8c
Joel <dynamic.cast99@gmail.com>
2017-05-11 10:23:44 +0200

    Started controller implementing

--- gunbird/ModuleInput.cpp
+++ gunbird/ModuleInput.cpp
@@ -26,6 +26,19 @@ bool ModuleInput::Init()
 		ret = false;
 	}
 
+	controller = nullptr;
+	for (int i = 0; i < SDL_NumJoysticks(); ++i) {
+		if (SDL_IsGameController(i)) {
+			controller = SDL_GameControllerOpen(i);
+			if (controller) {
+				break;
+			}
+			else {
+				LOG("Could not open gamecontroller %i: %s\n", i, SDL_GetError());
+			}
+		}
+	}
+
 	return ret;
 }
 
@@ -54,6 +67,8 @@ update_status ModuleInput::PreUpdate()
 		}
 	}
 
+	inputGamepad();
+
 	if (keyboard[SDL_SCANCODE_ESCAPE])
 		return update_status::UPDATE_STOP;
 
@@ -65,5 +80,110 @@ bool ModuleInput::CleanUp()
 {
 	LOG("Quitting SDL input event subsystem");
 	SDL_QuitSubSystem(SDL_INIT_EVENTS);
+
+	for (int i = 0; i < SDL_NumJoysticks(); ++i) {
+		if (SDL_IsGameController(i)) {
+			SDL_GameController *controller = SDL_GameControllerOpen(i);
+			if (controller) {
+				SDL_GameControllerClose(controller);
+			}
+			else {
+				LOG("Could not open gamecontroller %i: %s\n", i, SDL_GetError());
+			}
+		}
+	}
+
 	return true;
+}
+
+void ModuleInput::inputGamepad() {
+	//A
+	if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_A) == 1) {
+		if (gamepad.A == BUTTON_IDLE)
+			gamepad.A = BUTTON_DOWN;
+		else
+			gamepad.A = BUTTON_REPEAT;
+	}
+	else
+	{
+		if (gamepad.A == BUTTON_REPEAT || (gamepad.A == BUTTON_DOWN))
+			gamepad.A = BUTTON_UP;
+		else
+			gamepad.A = BUTTON_IDLE;
+	}
+
+	//X
+	if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_A) == 1) {
+		if (gamepad.A == BUTTON_IDLE)
+			gamepad.A = BUTTON_DOWN;
+		else
+			gamepad.A = BUTTON_REPEAT;
+	}
+	else
+	{
+		if (gamepad.A == BUTTON_REPEAT || (gamepad.A == BUTTON_DOWN))
+			gamepad.A = BUTTON_UP;
+		else
+			gamepad.A = BUTTON_IDLE;
+	}
+
+	//START
+	if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_START) == 1) {
+		if (gamepad.START == BUTTON_IDLE)
+			gamepad.START = BUTTON_DOWN;
+		else
+			gamepad.START = BUTTON_REPEAT;
+	}
+	else
+	{
+		if (gamepad.START == BUTTON_REPEAT || (gamepad.START == BUTTON_DOWN))
+			gamepad.START = BUTTON_UP;
+		else
+			gamepad.START = BUTTON_IDLE;
+	}
+
+	//LEFT STICK AXIS X POSITIVE
+	if (SDL_GameControllerGetAxis(controller, SDL_CONTROLLER_AXIS_LEFTX) > 8191) {
+		if (gamepad.LEFT_AXIS_POS_X == BUTTON_IDLE)
+			gamepad.LEFT_AXIS_POS_X = BUTTON_DOWN;
+		else
+			gamepad.LEFT_AXIS_POS_X = BUTTON_IDLE;
+	}
+	else
+	{
+		if (gamepad.LEFT_AXIS_POS_X == BUTTON_REPEAT || gamepad.LEFT_AXIS_POS_X == BUTTON_DOWN)
+			gamepad.LEFT_AXIS_POS_X = BUTTON_UP;
+		else
+			gamepad.LEFT_AXIS_POS_X = BUTTON_IDLE;
+	}
+
+	//LEFT STICK AXIS X NEGATIVE
+	if (SDL_GameControllerGetAxis(controller, SDL_CONTROLLER_AXIS_LEFTX) < -8191) {
+		if (gamepad.LEFT_AXIS_NEG_X == BUTTON_IDLE)
+			gamepad.LEFT_AXIS_NEG_X = BUTTON_DOWN;
+		else
+			gamepad.LEFT_AXIS_NEG_X = BUTTON_IDLE;
+	}
+	else
+	{
+		if (gamepad.LEFT_AXIS_NEG_X == BUTTON_REPEAT || gamepad.LEFT_AXIS_POS_X == BUTTON_DOWN)
+			gamepad.LEFT_AXIS_NEG_X = BUTTON_UP;
+		else
+			gamepad.LEFT_AXIS_NEG_X = BUTTON_IDLE;
+	}
+
+	//LEFT STICK AXIS Y POSITIVE
+	if (SDL_GameControllerGetAxis(controller, SDL_CONTROLLER_AXIS_LEFTY) > 8191) {
+		if (gamepad.LEFT_AXIS_POS_Y == BUTTON_IDLE)
+			gamepad.LEFT_AXIS_POS_Y = BUTTON_DOWN;
+		else
+			gamepad.LEFT_AXIS_POS_Y = BUTTON_IDLE;
+	}
+	else
+	{
+		if (gamepad.LEFT_AXIS_POS_Y == BUTTON_REPEAT || gamepad.LEFT_AXIS_POS_X == BUTTON_DOWN)
+			gamepad.LEFT_AXIS_POS_Y = BUTTON_UP;
+		else
+			gamepad.LEFT_AXIS_POS_Y = BUTTON_IDLE;
+	}
 }
--- gunbird/ModuleInput.h
+++ gunbird/ModuleInput.h
@@ -15,6 +15,22 @@ enum KEY_STATE
 	KEY_UP
 };
 
+enum CONTROLLER_STATE {
+	BUTTON_IDLE = 0,
+	BUTTON_DOWN,
+	BUTTON_REPEAT,
+	BUTTON_UP
+};
+
+struct Gamepad {
+	CONTROLLER_STATE A;
+	CONTROLLER_STATE X;
+	CONTROLLER_STATE START;
+	CONTROLLER_STATE LEFT_AXIS_POS_X;
+	CONTROLLER_STATE LEFT_AXIS_NEG_X;
+	CONTROLLER_STATE LEFT_AXIS_POS_Y;
+	CONTROLLER_STATE LEFT_AXIS_NEG_Y;
+};
 
 class ModuleInput : public Module
 {
@@ -26,9 +42,13 @@ public:
 	bool Init();
 	update_status PreUpdate();
 	bool CleanUp();
+	void inputGamepad();
 
-public:
 	KEY_STATE keyboard[MAX_KEYS];
+	Gamepad gamepad;
+
+private:
+	SDL_GameController* controller;
 };
 
 #endif // __ModuleInput_H__
