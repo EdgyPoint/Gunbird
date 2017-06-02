f0e3bd5344342edea15c1b7b8e56dbbf541ef17f
Joel <dynamic.cast99@gmail.com>
2017-06-02 18:34:10 +0200

    Green robot and bomb in progress

---
+++ gunbird/Enemy_GreenRobot.h
@@ -0,0 +1,22 @@
+//#ifndef __ENEMY_GREENROBOT_H__
+//#define __ENEMY_GREENROBOT_H__
+//
+//#include "Enemy.h"
+//#include "Path.h"
+//
+//class Enemy_GreenRobot : public Enemy
+//{
+//private:
+//	iPoint original_pos;
+//	fPoint distance;
+//	Animation stand;
+//	Animation walk_backward;
+//	Path path;
+//public:
+//	Enemy_GreenRobot(int x, int y, int option);
+//	void Move();
+//	int initcounter = 0;
+//	float aux_float;
+//	fPoint speeds;
+//};
+//#endif
